d69f87a0d56e918cff5b5249478a0285ea810067
tragacheyskiy <dimon.st.98@gmail.com>
2020-05-04 19:56:54 +0500

    Get Started

--- MealRandomizer/MealRandomizer/App.xaml.cs
+++ MealRandomizer/MealRandomizer/App.xaml.cs
@@ -10,7 +10,7 @@ namespace MealRandomizer
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
---
+++ MealRandomizer/MealRandomizer/Views/UserViewPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MealRandomizer.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserViewPage : ContentPage
+    {
+        public UserViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
